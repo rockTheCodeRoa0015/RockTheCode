722cd65e8c0789c22a7e626809835f5fe5ca18f2
Jesus <roa0015@gmail.com>
2023-12-22 09:45:17 +0100

    ejemplo-vite
    primer proyecto en vite

---
+++ ejemplo-vite/modules.js
@@ -0,0 +1,11 @@
+export const person = {
+  name: 'Stephen',
+  surname: 'Strange'
+}
+
+export const sayHello = () => {
+  console.log('Hello!')
+}
+const DEV_LAN = 'JavaScript'
+
+export default DEV_LAN
